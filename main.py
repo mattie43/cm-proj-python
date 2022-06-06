183378b63285f2e2e404fcaa8a5579ab9d56780b
mattie43 <mattericksen93@gmail.com>
2022-06-05 22:56:11 -0400

    added home

--- main.py
+++ main.py
@@ -40,6 +40,11 @@ class Video(Resource):
       return{'message':'failed'}
     return{'message':data}
 
+class Home(Resource):
+  def get(self):
+    return {'message': 'welcome to homepage'}
+
+api.add_resource(Home, "/")
 api.add_resource(Video, "/video/<int:video_id>")
 
 if __name__ == "__main__":
